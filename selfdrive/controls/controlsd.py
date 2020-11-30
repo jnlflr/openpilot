2327d7c44f6d34803d0f0e4fb5252236883ff459
flwrpwr <jean.lafleur@outlook.fr>
2020-11-30 18:34:01 +0100

    lanes on can7

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -379,9 +379,9 @@ def data_send(sm, pm, CS, CI, CP, VM, state, events, actuators, v_cruise_kph, rk
     # send car controls over can
     can_sends = CI.apply(CC)
     idx = sm.frame % 4
-    lpoly3 = int(100 * sm['pathPlan'].lPoly[3])
+    lpoly3 = int(100.0 * sm['pathPlan'].lPoly[2])
     cloudlog.debug("l_poly %d" % lpoly3)
-    
+
     """
     z = [10000000,1000000,100000,10000]
     b = [30000,30000,30000,30000]
